bb6ea0a345436160bba77017e4dad4196b6da4d5
David Golden <dagolden@cpan.org>
2014-02-27 11:31:06 -0500

    tidy all code

--- lib/App/CPAN/Mini/Visit.pm
+++ lib/App/CPAN/Mini/Visit.pm
@@ -18,136 +18,133 @@ use Pod::Usage 1.35 qw/ pod2usage /;
 use Archive::Extract 0.28 ();
 
 my @option_spec = (
-  Switch("help|h"),
-  Switch("version|V"),
-  Switch("quiet|q"),
-  Param("append|a", qr/(?:^$|(?:^path|dist$))/ )->default(''),
-  Param("e|E"),
-  Param("minicpan|m"),
-  Param("output|o"),
+    Switch("help|h"), Switch("version|V"),
+    Switch("quiet|q"), Param( "append|a", qr/(?:^$|(?:^path|dist$))/ )->default(''),
+    Param("e|E"),      Param("minicpan|m"),
+    Param("output|o"),
 );
 
 sub run {
-  my ($self, @args) = @_;
+    my ( $self, @args ) = @_;
 
-  # get command line options
-  my $opt = try eval { Getopt::Lucid->getopt( \@option_spec, \@args ) };
-  for ( catch ) {
-    if ( $_->isa('Getopt::Lucid::Exception::ARGV') ) {
-      print "$_\n"; 
-      # usage stuff
-      return 1;
-    }
-    else {
-      die $_ 
-    }
-  }
-
-  # handle "help" and "version" options
-  return _exit_usage() if $opt->get_help;
-  return _exit_version() if $opt->get_version;
-
-  # Set Archive::Extract globals
-  # if quiet suppress warnings from Archive::Tar, etc.
-  local $Archive::Extract::DEBUG = 0;
-  local $Archive::Extract::PREFER_BIN = 1;
-  local $Archive::Extract::WARN = $opt->get_quiet ? 0 : 1;
-
-  # if -e/-E, then prepend to command
-  if ( $opt->get_e ) {
-    unshift @args, $^X, ($^X > 5.009 ? '-E' : '-e'), $opt->get_e;
-  }
-
-  # locate minicpan directory
-  if ( ! $opt->get_minicpan ) {
-    my %config = CPAN::Mini->read_config;
-    if ( $config{local} ) {
-      $opt->merge_defaults( {minicpan => $config{local}} );
-    }
-  }
-
-  # confirm minicpan directory that looks like minicpan
-  return _exit_no_minicpan() if ! $opt->get_minicpan;
-  return _exit_bad_minicpan($opt->get_minicpan) if ! -d $opt->get_minicpan;
-
-  my $id_dir = dir($opt->get_minicpan, qw/authors id/);
-  return _exit_bad_minicpan($opt->get_minicpan) if ! -d $id_dir;
-
-  # process all distribution tarballs in authors/id/...
-  my $archive_re = qr{\.(?:tar\.(?:bz2|gz|Z)|t(?:gz|bz)|zip|pm\.gz)$}i;
-
-  my $minicpan = dir( $opt->get_minicpan )->absolute;
-  
-  # save output by redirecting STDOUT if requested
-  my ($out_fh, $orig_stdout );
-  if ( $opt->get_output ) {
-    open $out_fh, ">", $opt->get_output;
-    open $orig_stdout, "<&=STDOUT";
-    open STDOUT, ">&=" . fileno $out_fh;
-  }
-
-  find( 
-    {
-      no_chdir => 1,
-      follow => 0,
-      preprocess => sub { my @files = sort @_; return @files },
-      wanted => sub {
-        return unless /$archive_re/;
-        # run code if program/args given otherwise print name
-        if ( @args ) {
-          return if $_ =~ /pm\.gz$/io; # not an archive, just a file
-          my @cmd = @args;
-          if ( $opt->get_append ) {
-            if ( $opt->get_append eq 'dist' ) {
-              my $distname = $_;
-              my $prefix = dir( $minicpan, qw/authors id/ );
-              $distname =~ s{^$prefix[\\/].[\\/]..[\\/]}{};
-              push @cmd, $distname;
-            }
-            else {
-              push @cmd, $_;
-            }
-          }
-          _visit( $_, @cmd );
+    # get command line options
+    my $opt = try eval { Getopt::Lucid->getopt( \@option_spec, \@args ) };
+    for (catch) {
+        if ( $_->isa('Getopt::Lucid::Exception::ARGV') ) {
+            print "$_\n";
+            # usage stuff
+            return 1;
         }
         else {
-          print "$_\n"; 
+            die $_;
         }
-      },
-    },
-    $minicpan
-  );
+    }
 
-  # restore STDOUT and close output file
-  if ( $opt->get_output ) {
-    open STDOUT, ">&=" . fileno $orig_stdout;
-    close $out_fh;
-  }
+    # handle "help" and "version" options
+    return _exit_usage()   if $opt->get_help;
+    return _exit_version() if $opt->get_version;
 
-  return 0; # exit code
+    # Set Archive::Extract globals
+    # if quiet suppress warnings from Archive::Tar, etc.
+    local $Archive::Extract::DEBUG      = 0;
+    local $Archive::Extract::PREFER_BIN = 1;
+    local $Archive::Extract::WARN       = $opt->get_quiet ? 0 : 1;
+
+    # if -e/-E, then prepend to command
+    if ( $opt->get_e ) {
+        unshift @args, $^X, ( $^X > 5.009 ? '-E' : '-e' ), $opt->get_e;
+    }
+
+    # locate minicpan directory
+    if ( !$opt->get_minicpan ) {
+        my %config = CPAN::Mini->read_config;
+        if ( $config{local} ) {
+            $opt->merge_defaults( { minicpan => $config{local} } );
+        }
+    }
+
+    # confirm minicpan directory that looks like minicpan
+    return _exit_no_minicpan() if !$opt->get_minicpan;
+    return _exit_bad_minicpan( $opt->get_minicpan ) if !-d $opt->get_minicpan;
+
+    my $id_dir = dir( $opt->get_minicpan, qw/authors id/ );
+    return _exit_bad_minicpan( $opt->get_minicpan ) if !-d $id_dir;
+
+    # process all distribution tarballs in authors/id/...
+    my $archive_re = qr{\.(?:tar\.(?:bz2|gz|Z)|t(?:gz|bz)|zip|pm\.gz)$}i;
+
+    my $minicpan = dir( $opt->get_minicpan )->absolute;
+
+    # save output by redirecting STDOUT if requested
+    my ( $out_fh, $orig_stdout );
+    if ( $opt->get_output ) {
+        open $out_fh, ">", $opt->get_output;
+        open $orig_stdout, "<&=STDOUT";
+        open STDOUT, ">&=" . fileno $out_fh;
+    }
+
+    find(
+        {
+            no_chdir   => 1,
+            follow     => 0,
+            preprocess => sub { my @files = sort @_; return @files },
+            wanted     => sub {
+                return unless /$archive_re/;
+                # run code if program/args given otherwise print name
+                if (@args) {
+                    return if $_ =~ /pm\.gz$/io; # not an archive, just a file
+                    my @cmd = @args;
+                    if ( $opt->get_append ) {
+                        if ( $opt->get_append eq 'dist' ) {
+                            my $distname = $_;
+                            my $prefix = dir( $minicpan, qw/authors id/ );
+                            $distname =~ s{^$prefix[\\/].[\\/]..[\\/]}{};
+                            push @cmd, $distname;
+                        }
+                        else {
+                            push @cmd, $_;
+                        }
+                    }
+                    _visit( $_, @cmd );
+                }
+                else {
+                    print "$_\n";
+                }
+            },
+        },
+        $minicpan
+    );
+
+    # restore STDOUT and close output file
+    if ( $opt->get_output ) {
+        open STDOUT, ">&=" . fileno $orig_stdout;
+        close $out_fh;
+    }
+
+    return 0; # exit code
 }
 
 sub _exit_no_minicpan {
-  print STDERR << "END_NO_MINICPAN";
+    print STDERR << "END_NO_MINICPAN";
 No minicpan configured.
 
 END_NO_MINICPAN
-  return 1;
+    return 1;
 }
 
 sub _exit_bad_minicpan {
-  my ($dir) = @_;
-  die "requires directory argument" unless defined $dir;
-  print STDERR << "END_BAD_MINICPAN";
+    my ($dir) = @_;
+    die "requires directory argument" unless defined $dir;
+    print STDERR << "END_BAD_MINICPAN";
 Directory '$dir' does not appear to be a CPAN repository.
 
 END_BAD_MINICPAN
-  return 1;
+    return 1;
 }
 
 sub _exit_usage {
-  my $exe = basename($0);
-  print STDERR << "END_USAGE";
+    my $exe = basename($0);
+    print STDERR << "END_USAGE";
 Usage:
   $exe [OPTIONS] [PROGRAM]
 
@@ -175,56 +172,56 @@ Options:
 
 
 END_USAGE
-  return 1;
+    return 1;
 }
 
 sub _exit_version {
-  print STDERR basename($0) . ": $VERSION\n";
-  return 1
+    print STDERR basename($0) . ": $VERSION\n";
+    return 1;
 }
 
 sub _visit {
-  my ($archive, @cmd_line) = @_;
-  
-  my $tempd = tempd;
+    my ( $archive, @cmd_line ) = @_;
 
-  my $ae = Archive::Extract->new( archive => $archive );
+    my $tempd = tempd;
 
-  my $olderr;
+    my $ae = Archive::Extract->new( archive => $archive );
 
-  # stderr > /dev/null if quiet
-  if ( ! $Archive::Extract::WARN ) {
-    open $olderr, ">&STDERR";
-    open STDERR, ">", File::Spec->devnull;
-  }
+    my $olderr;
 
-  my $extract_ok = $ae->extract;
+    # stderr > /dev/null if quiet
+    if ( !$Archive::Extract::WARN ) {
+        open $olderr, ">&STDERR";
+        open STDERR, ">", File::Spec->devnull;
+    }
 
-  # restore stderr if quiet
-  if ( ! $Archive::Extract::WARN ) {
-    open STDERR, ">&", $olderr;
-    close $olderr;
-  }
+    my $extract_ok = $ae->extract;
 
-  if ( ! $extract_ok ) {
-    warn "Couldn't extract '$archive'\n" if $Archive::Extract::WARN;
+    # restore stderr if quiet
+    if ( !$Archive::Extract::WARN ) {
+        open STDERR, ">&", $olderr;
+        close $olderr;
+    }
+
+    if ( !$extract_ok ) {
+        warn "Couldn't extract '$archive'\n" if $Archive::Extract::WARN;
+        return;
+    }
+
+    # most distributions unpack a single directory that we must enter
+    # but some behave poorly and unpack to the current directory
+    my @children = dir()->children;
+    if ( @children == 1 && -d $children[0] ) {
+        chdir $children[0];
+    }
+
+    # execute command
+    my $rc = system(@cmd_line);
+    if ( $rc == -1 ) {
+        warn "Error running '@cmd_line': $!\n";
+    }
+
     return;
-  }
-  
-  # most distributions unpack a single directory that we must enter
-  # but some behave poorly and unpack to the current directory
-  my @children = dir()->children;
-  if ( @children == 1 && -d $children[0] ) {
-    chdir $children[0];
-  }
-  
-  # execute command
-  my $rc = system( @cmd_line );
-  if ( $rc == -1 ) {
-    warn "Error running '@cmd_line': $!\n";
-  }
-
-  return;
 }
 
 1;
--- t/CPAN/update-checksums.pl
+++ t/CPAN/update-checksums.pl
@@ -4,12 +4,12 @@ use warnings;
 use CPAN::Checksums qw/updatedir/;
 use File::Find;
 
-sub wanted { 
-    return unless 
-        -d $File::Find::name &&
-        $File::Find::name =~ m{authors/id/./../.+$};
-    print "$File::Find::name\n";    
-    updatedir( $File::Find::name );
+sub wanted {
+    return
+      unless -d $File::Find::name
+      && $File::Find::name =~ m{authors/id/./../.+$};
+    print "$File::Find::name\n";
+    updatedir($File::Find::name);
 }
 
 find( { wanted => \&wanted, no_chdir => 1 }, q{.} );
